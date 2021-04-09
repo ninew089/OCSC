0f3b9e83f29ddf128fe32b8ba9417236be6eb284
ninew.jck <ninew.jck@gmai.com>
2021-04-09 18:54:35 +0700

    fix

---
+++ build/precache-manifest.30c1bdc2b5854eaa036fd0587d79f542.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6669625d3c67f68fbc2f745f9eca6037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce619a7d1f2f8c8a4f5",
+    "url": "/static/css/main.5962d8dc.chunk.css"
+  },
+  {
+    "revision": "1ecefcf136002818f9e9",
+    "url": "/static/js/2.06690c09.chunk.js"
+  },
+  {
+    "revision": "28dd8245e7cef7e39a13b7e5136d388c",
+    "url": "/static/js/2.06690c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce619a7d1f2f8c8a4f5",
+    "url": "/static/js/main.a4b9dfe6.chunk.js"
+  },
+  {
+    "revision": "0467cf6e03846fdf98aa",
+    "url": "/static/js/runtime-main.360ed79e.js"
+  },
+  {
+    "revision": "114085097894615844558822c9f1af92",
+    "url": "/static/media/001-02.11408509.png"
+  },
+  {
+    "revision": "d23a216b8eca9bec437272c1b40783d3",
+    "url": "/static/media/OCSC-banner.d23a216b.png"
+  },
+  {
+    "revision": "4f02c0a4758ec002f48a4940a525a50f",
+    "url": "/static/media/logo.4f02c0a4.png"
+  },
+  {
+    "revision": "e23b2c3013b781b1352a6698af243e3e",
+    "url": "/static/media/ocsc-logo_7.e23b2c30.png"
+  },
+  {
+    "revision": "c5081be2a29e4bbdf4c14a89109a0f2a",
+    "url": "/static/media/transfer-to-ocsc.c5081be2.png"
+  }
+]);
